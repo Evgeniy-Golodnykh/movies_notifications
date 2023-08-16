7975ad73616001ba6213080c7db37206402a37de
Evgeniy Golodnykh <evgenferrum@gmail.com>
2023-08-17 00:19:14 +0300

    debug false

--- parser/configs.py
+++ parser/configs.py
@@ -5,7 +5,7 @@ from constants import (
     POSTGRES_PASSWORD, POSTGRES_USER,
 )
 
-DEBUG = True
+DEBUG = False
 LOGFORMAT = '%(asctime)s [%(levelname)s] %(filename)s/%(funcName)s %(message)s'
 
 POSTGRES_DB = {
