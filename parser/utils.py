24ce81888102adfbd8c23eaf20d4c48edd4daea8
Evgeniy Golodnykh <evgenferrum@gmail.com>
2023-08-17 15:36:24 +0300

    debug options

--- parser/utils.py
+++ parser/utils.py
@@ -16,6 +16,8 @@ def get_movies():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--ignore-certificate-errors')
     options.add_argument('--headless')
     options.add_argument('--window-size=1920,1080')
     try:
