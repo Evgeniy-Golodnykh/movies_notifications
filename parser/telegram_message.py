ae8983a3c00c2e19de18334aff474d329bd40f61
Evgeniy Golodnykh <113941793+Evgeniy-Golodnykh@users.noreply.github.com>
2024-07-02 11:35:42 +0300

    add parse mode to telegram message

--- parser/telegram_message.py
+++ parser/telegram_message.py
@@ -13,4 +13,4 @@ async def send_message(message):
 
     if check_tokens():
         async with telegram.Bot(token=TELEGRAM_TOKEN) as bot:
-            await bot.send_message(TELEGRAM_USER, message)
+            await bot.send_message(TELEGRAM_USER, message, parse_mode='MarkdownV2')
